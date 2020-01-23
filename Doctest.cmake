46ad051aad38b947e3202417cf851c80c4497e03
Emil Gedda <emil.gedda@emilgedda.se>
2020-01-23 01:22:50 +0100

    Initial commit

---
+++ Doctest.cmake
@@ -0,0 +1,19 @@
+include(ExternalProject)
+find_package(Git REQUIRED)
+
+ExternalProject_Add(
+    projdoctest
+    PREFIX ${CMAKE_BINARY_DIR}/doctest
+    GIT_REPOSITORY https://github.com/onqtam/doctest.git
+    UPDATE_COMMAND ${GIT_EXECUTABLE} pull
+)
+
+# Expose required variable (DOCTEST_INCLUDE_DIR) to parent scope
+ExternalProject_Get_Property(projdoctest source_dir)
+
+set(DOCTEST_INCLUDE_DIR ${source_dir}/doctest CACHE INTERNAL "Path to include folder for doctest")
+add_library(doctest INTERFACE)
+
+add_dependencies(doctest projdoctest)
+target_compile_features(doctest INTERFACE cxx_std_17)
+target_include_directories(doctest INTERFACE ${DOCTEST_INCLUDE_DIR})
